6c724974fbc6b98a6593229b381227cc00501e6c
Jonathan Dahnberg <Jonathan.MeeprongDahnberg@utb.ecutbildning.se>
2019-04-17 18:01:33 +0200

    grid added

--- connect_four/src/App.js
+++ connect_four/src/App.js
@@ -1,31 +1,37 @@
-import React, { Component } from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
+import Grid from "./components/field";
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      player: "red",
-      field: {},
-    }
+const grid = Array(6)
+    .fill(null)
+    .map(_ => Array(7).fill("white"));
 
-  }
+console.log(grid);
 
-  onClick(){
 
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <main className="App__main">
-          <div className="App__main__field">
-            
-          </div>
-        </main>
-      </div>
-    );
+function reducer(state, action){
+  switch(action.type){
+    case "fill_cell":
+    console.log(action.row);
+    console.log(action.column);
   }
 }
+
+const App = ()=>{
+const [state, dispatch] = useReducer(reducer, { selectedColor: "red", grid });
+
+//går inte att köra state.grid?
+
+  return (
+    <div className="App">
+      <main className="App__main">
+        <div className="App__main__field">
+          <Grid grid={grid} onClickCell={(row, column) => dispatch({ type: "fill_cell", row, column })}></Grid>
+        </div>
+      </main>
+    </div>
+  );
+}
+
 
 export default App;
--- connect_four/src/components/field.js
+++ connect_four/src/components/field.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../App.css';
 
 
-let Field = (props) => {
 
-    function returnField(obj){
-        return 
-    }
-
+let Grid = (props) => {
     return (
-        <div>
-            {props.state.field.map(obj => returnField(obj))}
+        <div className="Grid">
+            {props.grid.map((row, i) => {
+
+                let id = '_' + Math.random().toString(36).substr(2, 9);
+
+                return(
+                    <div className="Grid__row" key={id}>
+                        {row.map((cell, j) =>{
+      
+                            let id = '_' + Math.random().toString(36).substr(2, 9);
+                            
+                            return(
+                                <div className="Grid__row__cell" key={id} style={{ background: cell }} onClick={(()=>props.onClickCell(i, j))}></div>
+                            )
+                        })}
+                    </div>
+                )
+            })};
         </div>
     )
-
 }
 
-export default Field;
+export default Grid;
