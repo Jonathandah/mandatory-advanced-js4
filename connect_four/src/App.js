1def7ed05cfaaa0f31eee73e38156d3b2a5f8361
Jonathan Dahnberg <Jonathan.MeeprongDahnberg@utb.ecutbildning.se>
2019-04-25 22:18:55 +0200

    final

--- connect_four/src/App.js
+++ connect_four/src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useEffect } from 'react';
+import React, { useReducer} from 'react';
 import './App.css';
 import Grid from "./components/field";
 
@@ -6,18 +6,14 @@ const grid = Array(7)
     .fill(null)
     .map(_ => Array(6).fill("white"));
 
-console.log(grid);
-//grid[0][5] = "black";
 function checkLine(a,b,c,d){
   return((a !== "white") && (a ===b) && (a === c) && (a === d))
 }
-
 
 function reducer(state, action){
   switch(action.type){
 
     case "fill_cell":
-
 
     for(let i = action.rowArray.length-1; i>=0; i--){
       if(action.rowArray[i] === "white"){
@@ -37,6 +33,7 @@ function reducer(state, action){
     }
 
     case "reset_grid":
+
       let newGrid =  Array(7)
       .fill(null)
       .map(_ => Array(6).fill("white"));
@@ -49,6 +46,7 @@ function reducer(state, action){
       }
 
       case "check_winner":
+
         let grid = [...state.grid];
 
         //check column
@@ -58,7 +56,7 @@ function reducer(state, action){
               console.log(grid[c][r], "won");
               return{
                 ...state,
-                winner: grid[c][r],
+                winner: grid[c][r] + " won!",
               }
             }
           }
@@ -69,7 +67,7 @@ function reducer(state, action){
             if(checkLine(grid[c][r], grid[c+1][r], grid[c+2][r], grid[c+3][r])){
               return{
                 ...state,
-                winner: grid[c][r],
+                winner: grid[c][r] + " won!",
             }
           }
         }
@@ -81,7 +79,7 @@ function reducer(state, action){
           if(checkLine(grid[c][r], grid[c+1][r+1], grid[c+2][r+2], grid[c+3][r+3])){
             return{
               ...state,
-              winner: grid[c][r],
+              winner: grid[c][r] + " won!",
             }
           }
         }
@@ -93,7 +91,7 @@ function reducer(state, action){
           if(checkLine(grid[c][r], grid[c+1][r-1], grid[c+2][r-2], grid[c+3][r-3])){
             return{
               ...state,
-              winner: grid[c][r],
+              winner: grid[c][r] + " won!",
             }
           }
         }
@@ -113,7 +111,7 @@ function reducer(state, action){
 
       return{
         ...state,
-        winner: "draw",
+        winner: "Draw!",
       }
   }
 }
@@ -125,10 +123,12 @@ const [state, dispatch] = useReducer(reducer, { selectedColor: "red", draw: fals
   return (
     <div className="App">
       <main className="App__main">
-        <p>{state.winner}</p>
-        <button onClick={()=> dispatch({type: "reset_grid"})}>Restart</button>
+        <div className="App__main__container">
+          <h1>{state.winner}</h1>
+          <button onClick={()=> dispatch({type: "reset_grid"})}>Restart</button>
+        </div>
         <div className="App__main__field">
-          <Grid grid={state.grid}  onClickCell={(row, column, rowArray) => {
+          <Grid grid={state.grid} onClickCell={(row, column, rowArray) => {
               if(state.winner ===""){
                 dispatch({ type: "fill_cell", row, column, rowArray });
                 dispatch({type: "check_winner"});
